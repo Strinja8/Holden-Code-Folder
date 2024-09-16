b023402eb211006d8755208b73329b831cbe7709
Strinja8 <strinjah@gmail.com>
2024-09-16 09:49:11 -0600

    Emoticon art
    Emoticon Art

---
+++ 1-Demos and Examples/Lesson4Demo/src/App.java
@@ -0,0 +1,13 @@
+// Project: Lesson 4 Demo
+// Name: Holden Strinja
+// Date: Spet. 13, 2024
+// Description: Going over comments and special characters
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Wor\\ld!");
+    System.out.println("How was your day?");
+    System.out.println("It\'s \"skibidi\" with a chance of hawk tuah!");
+    }
+}
---
+++ A4 ASCII Art/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    System.out.println("    /\\----/\\");
+    System.out.println("    \\ .  . /");
+    System.out.println("    |\\    /|");
+    System.out.println("   /  \\  /  \\");
+    System.out.println("  /   >()<   \\");
+    System.out.println(" |   ______   |");
+    System.out.println(" | >\\ 0  0/<  | \t __");
+    System.out.println(" |   \\  0/    | \t/ /");
+    System.out.println(" |    \\0/     | \t /");
+    System.out.println(" /MMM\\    /MMM\\ \t/");
+    System.out.println("|  O  |__|  O  |_______/ ");
+    System.out.println("\\_____/  \\_____/");
+}
+}
