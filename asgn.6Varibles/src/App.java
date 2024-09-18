2136303b0a3df9b7bba4094c1a3220469a963e42
Strinja8 <strinjah@gmail.com>
2024-09-18 09:35:22 -0600

    ChickenWing assingment

---
+++ asgn.6Varibles/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    double wing = 4.75;
+    double cost = 0.75;
+     double total = 0;
+    total = (wing / cost);
+
+    System.out.print("You can Buy");
+    }
+}
