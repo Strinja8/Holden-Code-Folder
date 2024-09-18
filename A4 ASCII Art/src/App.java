2bd23caa609859f11b9db074750c995dd3fee595
Strinja8 <strinjah@gmail.com>
2024-09-18 08:52:21 -0600

    Mondays Work

--- A4 ASCII Art/src/App.java
+++ A4 ASCII Art/src/App.java
@@ -1,6 +1,10 @@
+/*Program Name: Rat ASCII Art
+ * Student Name: Holden Strinja
+ * Date: September 16th 2024
+ * Program Description: The programs function is to display a Rat holding a piece of cheese using symbols and characters on a keyboard.
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
     System.out.println("    /\\----/\\");
     System.out.println("    \\ .  . /");
     System.out.println("    |\\    /|");
