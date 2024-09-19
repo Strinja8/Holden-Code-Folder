553dec4903450dbf71b2654a33fc683714fb5b7e
Strinja8 <strinjah@gmail.com>
2024-09-19 09:48:06 -0600

    End of thursday

---
+++ L7.Strings/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // String name = new String("Holden");
+        // System.err.println(name.equals("holden"));
+
+        String message1 = "this is a test";
+
+        System.out.println(message1.length());
+
+        System.out.println(message1.substring(0, 5));
+        
+        System.out.println(message1.indexOf("hello"));
+    }
+}
