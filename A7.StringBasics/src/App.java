4a4f98eee4f90ec456cbcc10e752880ae354440b
Strinja8 <strinjah@gmail.com>
2024-09-20 09:31:48 -0600

    Part One of A7 (actually) Completed

--- A7.StringBasics/src/App.java
+++ A7.StringBasics/src/App.java
@@ -1,19 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
  {
-
+// Assingment Set up (Defining Strings)
         String message1 = "I am very happy!";
         String target1 = "very";
 
         String message2 = "That was great - lol.";
         String target2 = "lol";
-
+// Part One of Assingment
         int firNum = message1.indexOf("very");
-        System.out.println(firNum);
         int numLen = firNum + "very ".length();
-        System.out.println(numLen);
         int secNum = message1.length();
-        System.out.println(secNum);
         System.out.println(message1.substring(0,firNum) + message1.substring(numLen,secNum));
+// Part Two of assingment
+
 }   }
 }
