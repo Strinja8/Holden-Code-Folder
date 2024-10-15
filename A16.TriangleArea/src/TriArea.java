3a8135279226eef83f4521cab042ef32e2d4adba
Strinja8 <strinjah@gmail.com>
2024-10-15 08:59:30 -0600

    Area of a Triangle Finished

--- A16.TriangleArea/src/TriArea.java
+++ A16.TriangleArea/src/TriArea.java
@@ -1,9 +1,18 @@
+/*Triangle Area Calculator
+Holden Strinja
+October 15, 2024
+The program gets three side lengths of a triangle, then uses those side lengths to enter in the area of that triangle
+ */
+
+
 import java.util.Scanner;
 public class TriArea {
     
+    // Variables and Scanner initialized
     double a, b, c, p, s, AREA = 0.0;
     Scanner side = new Scanner(System.in);   
     
+    // Asking user for side lengths method
     public void nums(){
         System.out.print("Enter Side A: ");
         a = side.nextDouble();
@@ -13,12 +22,15 @@ public class TriArea {
         c = side.nextDouble();
     }
 
+    // Calculations for area method
     public void calc(){
         p = a + b + c;
         s = p/2;
         AREA = Math.sqrt(s*(s-a)*(s-b)*(s-c));
         
     }
+    
+    // Final Print method
     public void print(){
         System.out.printf("Area of the Triangle = " + "%.3f",AREA);
     }
--- A16.TriangleArea/src/TriAreaRunner.java
+++ A16.TriangleArea/src/TriAreaRunner.java
@@ -2,6 +2,7 @@ public class TriAreaRunner {
     public static void main(String[] args) throws Exception {
         TriArea aot = new TriArea();
 
+        // Running our methods
         aot.nums();
         aot.calc();
         aot.print();
