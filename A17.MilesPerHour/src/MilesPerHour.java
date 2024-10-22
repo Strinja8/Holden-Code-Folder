62580bbbcf3108209fe3bbdc31231f7f959634a9
Strinja8 <strinjah@gmail.com>
2024-10-22 09:01:46 -0600

    a17.MPH finished

--- A17.MilesPerHour/src/MilesPerHour.java
+++ A17.MilesPerHour/src/MilesPerHour.java
@@ -1,25 +1,36 @@
+/*
+Miles Per Hour Calculator
+Holden Strinja
+October 16, 2024
+The Program takes in a distance in miles, and a time in hours and minutes then converts those values to a speed in MPH to determine the rate you must trael to reach the distance in
+the given time
+ */
+
 import java.util.Scanner;
 
 public class MilesPerHour {
 double D, MPH, H, M, TH = 0.0;
 Scanner nums = new Scanner(System.in);
 
+//Method to get info from the user
 public void setinfo() {
 System.out.print("Enter the Distance: ");
 D = nums.nextDouble();
 System.out.print("Enter the Hours: ");
-H = nums.nextInt();
+H = nums.nextDouble();
 System.out.print("Enter the Minutes: ");
-M = nums.nextInt();
+M = nums.nextDouble();
 }
 
+//converting minutes to hours, and adding them to a Total Hours variable. then Calcualting our MPH
 public void calc() {
 TH = H + (M/60);
 MPH = D/TH;
 }
 
+//Printing out our MPH and other info
 public void print(){
-    System.out.printf( D + " Miles in " + H + " hours and " + M + " minutes = " + MPH + " Miles Per Hour!");
+    System.out.println( D + " Miles in " + H + " hours and " + M + " minutes = " + MPH + " Miles Per Hour!");
 }
 
 
--- A17.MilesPerHour/src/MilesPerHourRunner.java
+++ A17.MilesPerHour/src/MilesPerHourRunner.java
@@ -2,6 +2,7 @@ public class MilesPerHourRunner {
     public static void main(String[] args) throws Exception {
         MilesPerHour mph = new MilesPerHour();
 
+        // Running Code
         mph.setinfo();
         mph.calc();
         mph.print();
