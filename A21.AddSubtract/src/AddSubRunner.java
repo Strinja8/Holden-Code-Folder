350971953438a25babb83c88b9df9c92a94fd2a8
Strinja8 <strinjah@gmail.com>
2024-10-25 09:00:31 -0600

    A21.AddSub finished

--- A21.AddSubtract/src/AddSubRunner.java
+++ A21.AddSubtract/src/AddSubRunner.java
@@ -1,3 +1,10 @@
+/*
+Add and Subtract Calculator
+Holden Strinja
+October 25, 2024
+The program asks for two numbers a and b, if a is greater than b, a-b is printed. if b is greater than a ,b-a is printed. and if a and b are equal, a+b is printed
+ */
+
 import java.util.Scanner;
 
 public class AddSubRunner {
@@ -6,6 +13,7 @@ public class AddSubRunner {
     Scanner nums = new Scanner(System.in);
     int a, b;
 
+    // Method to get out a and b variables
     public void setnums() {
         System.out.print("Enter a Number: ");
         a = nums.nextInt();
@@ -13,6 +21,7 @@ public class AddSubRunner {
         b = nums.nextInt();
     }
 
+    // Conditionals Method to print out (a-b) if a>b, (b-a) if b>a, or (a+b) if a=b
     public void calc() {
         if (a > b) {
             System.out.println(a - b);
--- A21.AddSubtract/src/App.java
+++ A21.AddSubtract/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         AddSubRunner as = new AddSubRunner();
 
+        //Running the program
         as.setnums();
         as.calc();
     }
