3260755577cecdfcf2b8cad87251ce070cdfe6a8
Strinja8 <strinjah@gmail.com>
2024-11-01 09:10:17 -0600

    Finished A23.CoolNumbers

--- A23.CoolNumbers/src/App.java
+++ A23.CoolNumbers/src/App.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //Defining variables, runner, and scanner
         coolnumsRunner cn = new coolnumsRunner();
         boolean game = true;
         Scanner choice = new Scanner(System.in);
         String x = "";
         
+        //While loop running the program until ended by the user
         while (game) {
+            //Program
             cn.setnums();
             cn.calc();
             cn.print();
-            System.out.println("Would you like to play again? (yes or no)"); 
+            //Asks user if theyd want to go again
+            System.out.println("Would you like to go again? (yes or no)"); 
             x = choice.next();
-            if (x.equals("no")) {
+            //Conditional statement to exit the loop if chosen to stop playing
+            if (x.equals("no") || x.equals("No") || x.equals("NO") || x.equals("n") || x.equals("N")) {
+                System.out.println("Thank You For Playing!");
                 game = false;
             }
         }
--- A23.CoolNumbers/src/coolnumsRunner.java
+++ A23.CoolNumbers/src/coolnumsRunner.java
@@ -1,25 +1,37 @@
+/*
+Cool Numbers Calculator
+Holden Strinja
+November 1, 2024
+the program gets a max value from the user, the program then checks every number between 6 and the max value. For everynumber in between 6 and the max that has a remander of 1 when
+divided by 2, 3, 4, 5, and 6 it is considered a cool number and tallied up, it then prints the total amount of "cool numbers" in betweeb 6 and the max.
+ */
+
 import java.util.Scanner;
 
 public class coolnumsRunner {
 
+    //Defining variables and scanner
     Scanner nums = new Scanner(System.in);
     int max, count;
 
+    //method to get a max value from the user
     public void setnums() {
         System.out.print("\nEnter in your Maximum Number: ");
-        max = nums.nextInt();
-    }
+        max = nums.nextInt();  
+        }   
 
-    public void calc() {
-        for (int i = 6; i < max; i++) {
+    //Method to calculate total of "cool numbers"
+        public void calc() {
+        for (int i = 6; i < max; i++) { //For loop to check every number between 6 and the max
             if (i % 3 == 1 && i % 4 == 1 && i % 5 == 1 && i % 6 == 1) {
-                count++;
+                count++; // Adding to the total cool numbers counter
             }
         }
     }
 
+   // Final Print Method
     public void print() {
         System.out.println("There are " + count + " cool numbers between 6 - " + max);
-        count = 0;
+        count = 0; // Resetting the count variable if played multiple times in a row
     }
 }
