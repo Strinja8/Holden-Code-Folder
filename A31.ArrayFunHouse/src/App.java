f55e7e40cb1c0fa1ae6fd38883c5eaa539e29f9f
Strinja8 <strinjah@gmail.com>
2024-11-28 09:13:19 -0600

    ArrayFunHouse work

--- A31.ArrayFunHouse/src/App.java
+++ A31.ArrayFunHouse/src/App.java
@@ -1,8 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
     arrayFHRunner fh = new arrayFHRunner();
+    int[] one = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
+    int[] two = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
+    fh.getsum(one, 3, 6);
+    fh.getsum(one, 2, 9);
+    fh.count(one, 4);
+    fh.count(one, 9);
+    fh.count(one, 7);
     
-    fh.arrA();
         
 
     }
--- A31.ArrayFunHouse/src/arrayFHRunner.java
+++ A31.ArrayFunHouse/src/arrayFHRunner.java
@@ -2,33 +2,25 @@ import java.util.Arrays;
 
 public class arrayFHRunner {
     
-    int[] a1 = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
-    int[] b1 = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
-    int sum = 0;
-    int fourcount = 0;
-    int sevencount = 0;
-    int ninecount = 0;
-  
-public void arrA() {
-    for (int i = 0; i <= a1.length-1; i++) {
-        sum = sum+a1[i];
-        if (a1[i] == 4){
-            fourcount++;
-        }
-        else if (a1[i] == 7) {
-            sevencount++;
-        }
-        else if (a1[i] == 9) {
-            ninecount++;
+    int[] one = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
+    int[] two = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
+    int sum, counter;
+
+public void getsum(int[] numArray, int start, int stop) {
+    for (int i = start; i <= stop; i++) {
+        sum += numArray[i];
+    }
+    System.out.println("The Sum of Numbers " + start + "-" + stop + " is: " + sum);
+}  
+
+public void count(int[] numArray, int value) {
+    for (int i = 0; i <= numArray.length-1; i++) {
+        if (numArray[i] == 0) {
+            counter++;
         }
     }
-    System.out.println(Arrays.toString(a1));
-    System.out.println("Sum of Array: " + sum);
-    System.out.println("# of 4s: " + fourcount);
-    System.out.println("# of 7s: " + sevencount);
-    System.out.println("# of 9s: " + ninecount);
-    }
-    
+    System.out.println("# of " + value + "s = " + counter);
+}
 }
 
 
